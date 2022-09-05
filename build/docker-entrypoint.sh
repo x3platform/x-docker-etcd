65af6387287584fb95d0f8f16011ceafe8e44fd4
yoshow <ruanyu@live.com>
2022-09-05 16:46:13 +0800

    ci:update docker-entrypoint.sh

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,9 +1,1 @@
-etcd --name=\${ETCD_NAME} \
---data-dir=\${ETCD_DATA_DIR} \
---listen-peer-urls=\${ETCD_LISTEN_PEER_URLS} \
---listen-client-urls=\${ETCD_LISTEN_CLIENT_URLS} \
---advertise-client-urls=\${ETCD_ADVERTISE_CLIENT_URLS} \
---initial-advertise-peer-urls=\${ETCD_INITIAL_ADVERTISE_PEER_URLS} \
---initial-cluster=\${ETCD_INITIAL_CLUSTER} \
---initial-cluster-token=\${ETCD_INITIAL_CLUSTER_TOKEN} \
---initial-cluster-state=\${ETCD_INITIAL_CLUSTER_STATE}
+etcd
